87965ceb69b83bd2430f55950cb8275e81cd3706
mihai <blagamihai2000@gmail.com>
2020-10-05 18:10:53 +0300

    Initial Commit

---
+++ test_IKP.m
@@ -0,0 +1,16 @@
+clear all;
+close all;
+clc
+
+%Calcul cinematica inversa
+
+%Date de intrare
+
+xp = 50
+yp = 70
+a = 100
+
+%Date de iesire
+
+q1 = sqrt(xp^2 + yp^2); 
+q2 = sqrt((a - xp)^2 + yp^2);
